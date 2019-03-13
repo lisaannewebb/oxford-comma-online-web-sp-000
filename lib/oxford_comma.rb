e0ef4def7058fd29e912be6a72b2de9bcbe71b31
Lisa Webb <lwebbnyc@outlook.com>
2019-03-13 14:41:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
-array="hippo,giraffe,monkey,horse"
-
-
 def oxford_comma(array)
-  array.split(",")
+  if array.size == 1
+    return array[0]
+  elsif array.size == 2
+    return array.join(" and ")
+  else
+    return array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
